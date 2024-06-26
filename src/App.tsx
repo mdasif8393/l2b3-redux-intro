4d874ecbbf75d5913102bd7da2ef0bf6d9230de4
mdasif8393 <md.asif.8393@gmail.com>
2024-06-26 12:16:18 +0600

    create increment and decrement counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
+import { decrement, increment } from "./redux/features/CounterSlice";
+import { RootState } from "./redux/store";
 
 function App() {
+  const { count } = useSelector((state: RootState) => state.counter);
+  const dispatch = useDispatch();
+
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="h-screen w-full flex justify-center items-center">
+      <div className="flex border border-purple-400 rounded-md bg-slate-100 p-10">
+        <button
+          onClick={() => dispatch(decrement())}
+          className="px-3 py-2 rounded-md bg-red-500 text-xl font-semibold text-white"
+        >
+          Decrement
+        </button>
+        <h1 className="text-3xl mx-10">{count}</h1>
+        <button
+          onClick={() => dispatch(increment())}
+          className="px-3 py-2 rounded-md bg-green-500 text-xl font-semibold text-white"
+        >
+          Increment
+        </button>
+      </div>
+    </div>
   );
 }
 
